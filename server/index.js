7b510ae54dbfa7e2eaf7cb1bb0b8aa4ce871bbc8
Dallin Breen <dallinbreen624@gmail.com>
2022-02-24 14:04:50 -0700

    added rollbar

--- server/index.js
+++ server/index.js
@@ -5,6 +5,25 @@ const app = express();
 
 app.use(express.json());
 
+// include and initialize the rollbar library with your access token
+const Rollbar = require("rollbar");
+const rollbar = new Rollbar({
+  accessToken: "9dce5bb2692c4f70820bd5679812911e",
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+});
+
+// record a generic message and send it to Rollbar
+rollbar.log("Hello world!");
+
+app.get("/", (req, res) => {
+  try {
+    res.send(path.join(__dirname, "../index.js"));
+  } catch (error) {
+    console.log(error);
+  }
+});
+
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../main/index.html"));
 });
@@ -16,17 +35,6 @@ app.get("/main/index.js", (req, res) => {
 app.get("/main/index.css", (req, res) => {
   res.sendFile(path.join(__dirname, "../main/index.css"));
 });
-
-// include and initialize the rollbar library with your access token
-const Rollbar = require("rollbar");
-const rollbar = new Rollbar({
-  accessToken: "9dce5bb2692c4f70820bd5679812911e",
-  captureUncaught: true,
-  captureUnhandledRejections: true,
-});
-
-// record a generic message and send it to Rollbar
-rollbar.log("Hello world!");
 
 const port = process.env.PORT || 4005;
 
